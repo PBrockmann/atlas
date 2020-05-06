5b375b0b906b0e56766cac97f500d80f5f897faa
PBrockmann <Patrick.Brockmann@lsce.ipsl.fr>
2020-05-06 12:28:28 +0200

    Adapted to python3

--- interMonitoringProd.py
+++ interMonitoringProd.py
@@ -21,19 +21,19 @@ for line in open(sys.argv[1]):
     li=line.strip()
     if len(li) != 0 and not li.startswith("#"):
         arr = line.rstrip().split('\t')
-	arr = [x for x in arr if x != '']
-	simuList.append(arr[0])
-	if len(arr) > 1 :
-		dateOffset.append(arr[1])
-	else:
-		dateOffset.append('""')
+        arr = [x for x in arr if x != '']
+        simuList.append(arr[0])
+        if len(arr) > 1 :
+            dateOffset.append(arr[1])
+        else:
+            dateOffset.append('""')
 
-print "################################################################################"
+print("################################################################################")
 os.system("cat " + sys.argv[1])
-print "################################################################################"
-print simuList
-print dateOffset
-print "################################################################################"
+print("################################################################################")
+print(simuList)
+print(dateOffset)
+print("################################################################################")
 
 #=============================================================
 #outputDir = "interMonitoring_" + str(os.getpid())
@@ -44,45 +44,45 @@ os.mkdir(outputDir + "/images")
 
 shutil.copy(sys.argv[1], outputDir + "/simuList.txt")
 
-print "Output directory: ", outputDir
-print "################################################################################"
+print("Output directory: ", outputDir)
+print("################################################################################")
 
 #=============================================================
 simuListOk = []
 setFiles = []
 for i,simu in enumerate(simuList):
-	print i+1, simu
-	try:
-        	file = urllib.urlopen(simu + '/MONITORING/files/catalog.xml')
-        	handler = file.read()
-        	catalogSoup = BeautifulSoup(handler, "lxml")
-        	s = set()
-        	for tag in catalogSoup.findAll('dataset') :
-        	        if tag['name'].endswith(".nc"):
-        	                s.add(tag['name'])
-		print "--> Nb of monitoring files found: ", len(s) 
-		if len(s) == 0:
-			print "----> Zero length, will be skipped"
-		else:
-			simuListOk.append(simu)
-        		setFiles.append(s)
-	except:
-		print "----> Read problem, will be skipped"
-print "################################################################################"
+    print(i+1, simu)
+    try:
+        file = urllib.request.urlopen(simu + '/MONITORING/files/catalog.xml')
+        handler = file.read()
+        catalogSoup = BeautifulSoup(handler, "lxml")
+        s = set()
+        for tag in catalogSoup.findAll('dataset') :
+            if tag['name'].endswith(".nc"):
+                s.add(tag['name'])
+        print("--> Nb of monitoring files found: ", len(s))
+        if len(s) == 0:
+            print("----> Zero length, will be skipped")
+        else:
+            simuListOk.append(simu)
+            setFiles.append(s)
+    except:
+        print("----> Read problem, will be skipped")
+print("################################################################################")
 
 filesInter = set.intersection(*setFiles)
 
 for i,simu in enumerate(simuListOk):
-	print simu
-print "################################################################################"
+    print(simu)
+print("################################################################################")
 
 filesInter = sorted(filesInter)
 
-print 'Number of common files: ', len(filesInter)
-print "################################################################################"
+print('Number of common files: ', len(filesInter))
+print("################################################################################")
 
 if len(filesInter) == 0:
-	sys.exit()
+    sys.exit()
 
 #sys.exit()
 
@@ -97,7 +97,7 @@ script = j2_env.get_template('interMonitoring.jnl.template').render(simuList=sim
 
 scriptFile = outputDir + "/interMonitoring.jnl"
 with open(scriptFile, "wb") as fh:
-    fh.write(script)
+    fh.write(script.encode())
 
 #=============================================================
 frameColors = {
@@ -116,21 +116,21 @@ quiet = " > /dev/null 2>&1"
 #for file in filesInter:
 for file in filesInter[20:24]:		# Only 4 first files for testing
 
-	color = frameColors[file.split('_')[0]]
-
-	cmd = "pyferret -quiet -noverify -batch " + outputDir + "/images/" + file.replace(".nc",".png") + " -script " + scriptFile + " " + file + \
-			 " " + smooth + " " + ' '.join(dateOffset)
-	print cmd
-	os.system(cmd + quiet)
-	
-	cmd = "convert " + outputDir + "/images/" + file.replace(".nc",".png") + " " + outputDir + "/images/" + file.replace(".nc",".gif")
-	print cmd
-	os.system(cmd + quiet)
-
-	cmd = "convert -geometry 50%x50% -bordercolor '" + color + "' -border 15x15 " + outputDir + "/images/" + file.replace(".nc",".png") + " " + \
-			outputDir + "/images/" + file.replace(".nc",".jpg")
-	print cmd
-	os.system(cmd + quiet)
+    color = frameColors[file.split('_')[0]]
+    
+    cmd = "pyferret -quiet -noverify -batch " + outputDir + "/images/" + file.replace(".nc",".png") + " -script " + scriptFile + " " + file + \
+    		 " " + smooth + " " + ' '.join(dateOffset)
+    print(cmd)
+    os.system(cmd + quiet)
+    
+    cmd = "convert " + outputDir + "/images/" + file.replace(".nc",".png") + " " + outputDir + "/images/" + file.replace(".nc",".gif")
+    print(cmd)
+    os.system(cmd + quiet)
+    
+    cmd = "convert -geometry 50%x50% -bordercolor '" + color + "' -border 15x15 " + outputDir + "/images/" + file.replace(".nc",".png") + " " + \
+    		outputDir + "/images/" + file.replace(".nc",".jpg")
+    print(cmd)
+    os.system(cmd + quiet)
 
 #=============================================================
 simuNames = [ os.path.basename(simu) for simu in simuListOk ]
@@ -138,8 +138,8 @@ simuNames = [ os.path.basename(simu) for simu in simuListOk ]
 titleTag = 'Inter-monitoring: ' + ' vs '.join(simuNames)
 title = 'Inter-monitoring: <ul style="font-size: 16px;">' + ''.join(['<li>' + s for s in simuNames]) + '</ul>'
 cmd = "monitoring01_createindex -t '" + title + "' --titleTag '" + titleTag + "' " + outputDir;
-print cmd
+print(cmd)
 os.system(cmd)
 
 #=============================================================
-print "################################################################################"
+print("################################################################################")
